ad10aef5ebb5b08edce52e2adf6f91bd62895da9
csant510 <james.sant83@gmail.com>
2020-11-21 19:13:33 -0800

    created the todos routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require('../models/todo');
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+*/
+
+
+router.get('/', function(req, res) {
+  res.render('todos/index', {
+    todos: Todo.getAll()
+  });
 });
 
 module.exports = router;
