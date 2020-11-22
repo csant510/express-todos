e2c9ae575d466fed061138846a2821988f1edf84
csant510 <james.sant83@gmail.com>
2020-11-21 20:06:23 -0800

    refactor to for a controller

---
+++ controllers/todos.js
@@ -0,0 +1,12 @@
+var Todo = require('../models/todo');
+
+module.exports ={
+    index
+};
+
+
+function index(req, res) {
+    res.render('todos/index', {
+      todos: Todo.getAll()
+    });
+  }
--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Todo = require('../models/todo');
-
-/* GET users listing. 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-*/
+var todosCtrl = require('../controllers/todos')
 
 
-router.get('/', function(req, res) {
-  res.render('todos/index', {
-    todos: Todo.getAll()
-  });
-});
+router.get('/',todosCtrl.index );
 
 module.exports = router;
